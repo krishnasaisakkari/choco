be3c124f78604e2ef6d7f6585965d3dfdda5503a
krishnasaisakkari <mr.krishnasaisakkari@gmail.com>
2024-04-06 09:41:08 +0530

    done

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 async function sum(){
     let store=await fetch("https://dummyjson.com/products");
     let data=await store.json();
-    // console.log(data.products);
+     console.log(data.products);
 
     let cards=document.querySelectorAll('.card');
     cards.forEach(function(item,i){
         item.getElementsByTagName('img')[0].src=data.products[i].images[0];
+        
     })
 }
 sum()

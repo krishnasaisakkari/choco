1adbf972ef1c7f6278c4ef4d2b8eb6b63f367de6
krishnasaisakkari <mr.krishnasaisakkari@gmail.com>
2024-05-23 07:46:08 +0530

    done

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-async function sum(){
-    let store=await fetch("https://dummyjson.com/products");
-    let data=await store.json();
-     console.log(data.products);
+// async function sum(){
+//     let store=await fetch("https://dummyjson.com/products");
+//     let data=await store.json();
+//      console.log(data.products);
 
-    let cards=document.querySelectorAll('.card');
-    cards.forEach(function(item,i){
-        item.getElementsByTagName('img')[0].src=data.products[i].images[0];
+//     let cards=document.querySelectorAll('.card');
+//     cards.forEach(function(item,i){
+//         item.getElementsByTagName('img')[0].src=data.products[i].images[0];
         
-    })
-}
-sum()
+//     })
+// }
+// sum()
+subM=document.querySelector('.filter');
+       let dataValue=document.querySelectorAll('.card');
+       dataValue.forEach(function(item){
+        subM.addEventListener('click', function(){
+            let m=document.getElementById("min").value;
+            let n=document.getElementById("max").value;
+            mValue=parseInt(m);
+            nValue=parseInt(n);
+             let dataItem=item.getAttribute("value");
+             if(dataItem >= mValue && dataItem <= nValue){
+                item.style.display="block";
+             }
+             else{
+                item.style.display="none";
+             }
+        })
+        
+       })
